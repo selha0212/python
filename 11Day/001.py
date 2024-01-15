84ce3844066d8f2b2cfb38e149f218647c73eadc
haeun <eiyap707@naver.com>
2024-01-16 08:49:31 +0900

    2024-01-15

---
+++ 11Day/001.py
@@ -0,0 +1,5 @@
+# https://hasaway2.github.io/python2/
+
+num=float(input('숫자 입력 : '))
+sosu = num * num
+print(f'제곱한 숫자 : {sosu}, 소수점 첫째자리 : {round(sosu,1)}')
---
+++ 11Day/003.py
@@ -0,0 +1,2 @@
+string = '동서남북동서남북동서남북'
+print(string[::4])
